d835e72b11fed370b56cfb5411f2c0fff6e600c9
glautrou <g.lautrou@outlook.com>
2018-07-05 21:57:05 +0200

    Create empty MVC website

---
+++ AspCoreSqlServerDockerLinux.Website/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AspCoreSqlServerDockerLinux.Website.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
